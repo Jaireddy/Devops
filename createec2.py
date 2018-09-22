ba9dde9c23537aec959a8e1fd1989d8a8dc56f0e
Jaireddy <jaipal.karam@gmail.com>
2018-09-22 15:06:53 +0000

    adding ec2

---
+++ createec2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+    ImageId='ami-a0cfeed8',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro')
+
+print instance[0].id
+
